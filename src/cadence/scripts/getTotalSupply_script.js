246be42d178284e1f5893927e2f16e7565a0d3ae
Genesis671230 <stashcloud671230@gmail.com>
2022-10-27 19:38:10 +0500

    rust flow on aptos

---
+++ src/cadence/scripts/getTotalSupply_script.js
@@ -0,0 +1,11 @@
+export const getTotalSupply =
+// REPLACE THIS WITH YOUR CONTRACT NAME + ADDRESS
+`
+import BottomShot from 0xe456c97e61b99e1c;
+
+pub fun main(): UInt64 {
+
+    return BottomShot.totalSupply;
+
+}
+`
